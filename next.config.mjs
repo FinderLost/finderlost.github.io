dbd42eadcd3def972ebdb68f94c6dd8e015d2873
Andrés Felipe <aalzres@gmail.com>
2023-04-07 19:48:23 +0100

    WIP

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     loader: 'akamai',
     path: '',
   },
   assetPrefix: './',
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
