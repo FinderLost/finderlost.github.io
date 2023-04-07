df45f7cdd1f7cc00d6ac8e66a0cd1cf629e12a66
Andrés Felipe <aalzres@gmail.com>
2023-04-07 11:28:04 +0100

    WIP

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const metadata = {
+  title: "Finder Lost",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import Navbar from '../components/Navbar/Navbar';
+import HomeSection from '../components/HomeSection/HomeSection';
+import KeyFeaturesSection from '../components/KeyFeaturesSection/KeyFeaturesSection';
+import DownloadSection from '../components/DownloadSection';
+import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Navbar />
+      <HomeSection />
+      <KeyFeaturesSection />
+      <DownloadSection />
+      <ContactSection />
+      <Footer />
+    </main>
+  );
+}
