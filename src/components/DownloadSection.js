4718b88b7917267e57bbf3ea47eb6212fd97f38e
Andrés Felipe <aalzres@gmail.com>
2023-04-07 16:16:39 +0100

    WIIP

--- src/components/DownloadSection.js
+++ src/components/DownloadSection.js
@@ -1,29 +1,35 @@
 const DownloadSection = () => {
-    return (
-      <section id="download" className="container mt-5">
-        <div className="row">
-          <div className="col-md-4">
-            <h2>App 1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis sapien at commodo commodo. Suspendisse potenti.</p>
-            <a href="https://apps.apple.com/us/app/finderlost/id1234567890" target="_blank" rel="noopener noreferrer"><img src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" alt="Download on the App Store" width="135" /></a>
-            <a href="https://play.google.com/store/apps/details?id=com.finderlost.app" target="_blank" rel="noopener noreferrer"><img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt="Get it on Google Play" width="135" /></a>
-          </div>
-          <div className="col-md-4">
-            <h2>App 2</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis sapien at commodo commodo. Suspendisse potenti.</p>
-            <a href="https://apps.apple.com/us/app/finderlost/id1234567890" target="_blank" rel="noopener noreferrer"><img src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" alt="Download on the App Store" width="135" /></a>
-            <a href="https://play.google.com/store/apps/details?id=com.finderlost.app" target="_blank" rel="noopener noreferrer"><img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt="Get it on Google Play" width="135" /></a>
-          </div>
-          <div className="col-md-4">
-            <h2>App 3</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis sapien at commodo commodo. Suspendisse potenti.</p>
-            <a href="https://apps.apple.com/us/app/finderlost/id1234567890" target="_blank" rel="noopener noreferrer"><img src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" alt="Download on the App Store" width="135" /></a>
-            <a href="https://play.google.com/store/apps/details?id=com.finderlost.app" target="_blank" rel="noopener noreferrer"><img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt="Get it on Google Play" width="135" /></a>
-          </div>
+  return (
+    <section id="download" className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-4 text-center">
+          <h2>FinderPet</h2>
+          <p className="text-center">
+            The ultimate solution for reuniting lost pets with their loving
+            owners. Join our community of animal lovers and help bring families
+            together again
+          </p>
         </div>
-      </section>
-    );
-  };
-  
-  export default DownloadSection;
-  
+        <div className="col-md-4 text-center">
+          <h2>FinderPeople</h2>
+          <p className="text-center">
+            Get connected with lost loved ones and join a global network of
+            seekers and finders. Our innovative technology and dedicated team
+            make it easier than ever to restore hope and bring people back
+            together.
+          </p>
+        </div>
+        <div className="col-md-4 text-center">
+          <h2>FinderThings</h2>
+          <p className="text-center">
+            Keep track of your most cherished belongings and never lose them
+            again. Our seamless platform connects seekers and finders across the
+            world, helping to bring possessions back to their rightful owners.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DownloadSection;
