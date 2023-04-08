9b45a970ba1b556d0b4f1ac413ea036fe090aa7b
Andrés Felipe <aalzres@gmail.com>
2023-04-08 16:57:17 +0100

    analytics implemented

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
+
+module.exports = nextConfig;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/images/logo.svg";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import { isSupported } from "@firebase/analytics";
 
 const Navbar = () => {
+  const handleLinkClick = (event, eventName) => {
+    if (typeof window !== "undefined" && isSupported()) {
+      event.preventDefault();
+      const analytics = getAnalytics();
+      logEvent(analytics, eventName, { link: event.target.href });
+      window.location.href = event.target.href;  
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container d-flex align-items-center">
         <div className="d-flex align-items-center">
-          <Link href="/">
+          <Link href="/" onClick={(e) => handleLinkClick(e, "logo_click")}>
             <Image
               src={logo}
               alt="Logo"
@@ -19,16 +30,32 @@ const Navbar = () => {
           <span className="ml-2 mx-2 text-base">Finder Lost</span>
         </div>
         <div className="navbar-nav mx-auto">
-          <a href="#" className="nav-link">
+          <a
+            href="#"
+            className="nav-link"
+            onClick={(e) => handleLinkClick(e, "home_click")}
+          >
             Home
           </a>
-          <a href="#key-features" className="nav-link">
+          <a
+            href="#key-features"
+            className="nav-link"
+            onClick={(e) => handleLinkClick(e, "features_click")}
+          >
             Features
           </a>
-          <a href="#apps" className="nav-link">
+          <a
+            href="#apps"
+            className="nav-link"
+            onClick={(e) => handleLinkClick(e, "apps_click")}
+          >
             Apps
           </a>
-          <a href="#contact" className="nav-link">
+          <a
+            href="#contact"
+            className="nav-link"
+            onClick={(e) => handleLinkClick(e, "contact_click")}
+          >
             Contact
           </a>
         </div>
