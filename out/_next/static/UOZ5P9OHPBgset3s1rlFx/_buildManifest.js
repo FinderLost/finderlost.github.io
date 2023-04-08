ade63e1adb86f86bebadab75922fdd692bc21f1a
Andrés Felipe <aalzres@gmail.com>
2023-04-08 16:57:17 +0100

    analytics implemented

---
+++ out/_next/static/UOZ5P9OHPBgset3s1rlFx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d20ab0fa56f60283.css","static/chunks/815-76a76088794b5b38.js","static/chunks/pages/index-ee6ca2a287cbc169.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
